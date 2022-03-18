d1ce77ea278cd444ce30b66dcea3f00d64c73729
v-giorgio <vitorlcardoso17@gmail.com>
2022-03-18 20:17:59 -0300

    feature: servidor configurado

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+
+const cors = require("cors");
+// const routes = require('./routes');
+
+// require('./database');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// routes(app);
+
+app.listen(port, () => {
+  try {
+    console.log(`Servindo na porta ${port}`);
+  } catch (err) {
+    console.log(`Erro ao servir: \n\n ${err}`);
+  }
+});
