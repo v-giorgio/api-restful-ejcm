9e26cf383b634afec4031e4c12817d156aa8d90f
v-giorgio <vitorlcardoso17@gmail.com>
2022-04-01 13:37:27 -0300

    feature: auth routes created

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,10 +1,20 @@
 const { Router } = require("express");
+const passport = require("passport");
 
 const Validations = require("../services/validations/validations");
 
+const AuthController = require("../controllers/AuthController");
 const UserController = require("../controllers/UserController");
 
 const userRouter = Router();
+
+/* login | authentication */
+
+// all routes with '/private' with have this middleware
+userRouter.use("/private", passport.authenticate("jwt", { session: false }));
+
+userRouter.get("/private/getDetails", AuthController.getDetails);
+userRouter.post("/login", AuthController.login);
 
 /* user routes */
 userRouter.get("/users", UserController.getAllUsers);
