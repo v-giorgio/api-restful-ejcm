ca52852d6a4eb5db01ec36ce2b672ca8500a03b7
v-giorgio <vitorlcardoso17@gmail.com>
2022-04-01 20:30:54 -0300

    feature: book seeder using external API

--- api/seeders/bookExamples.js
+++ api/seeders/bookExamples.js
@@ -1,63 +1,14 @@
 "use strict";
 
+const BookAPIController = require("../utils/BookAPIController");
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "Books",
-      [
-        {
-          title: "IT",
-          editor: "Suma",
-          release_year: "1986-09-15",
-          genre: "Terror",
-          user_id: 1,
-          author_id: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Me chame pelo seu nome",
-          editor: "Intrinseca",
-          release_year: "2018-01-05",
-          genre: "Romance",
-          user_id: 1,
-          author_id: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Variações Enigma",
-          editor: "Intrinseca",
-          release_year: "2017-01-03",
-          genre: "Romance",
-          user_id: 3,
-          author_id: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Misery",
-          editor: "Intrinseca",
-          release_year: "1987-06-08",
-          genre: "Suspense",
-          user_id: 2,
-          author_id: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Duna",
-          editor: "Aleph",
-          release_year: "1965-06-01",
-          genre: "Suspense",
-          user_id: 4,
-          author_id: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+    const bookArr = [];
+    for (let i = 0; i < 5; i++) {
+      bookArr.push(await BookAPIController.getBooks(i));
+    }
+    await queryInterface.bulkInsert("Books", bookArr, {});
   },
 
   async down(queryInterface, Sequelize) {
